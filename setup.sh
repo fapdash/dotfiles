acd4c726945eda381f8fc235a23b164689d145fc
fap <fapdash@users.noreply.github.com>
2019-02-12 22:38:34 +0100

    Add basic setup script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+################################
+######### Emacs ################
+################################
+ln -s ~/repos/dotfiles/emacs/.emacs ~/.emacs
+mkdir ~/.emacs.d 2> /dev/null
+ln -s ~/repos/dotfiles/emacs/lisp/ ~/.emacs.d/lisp
+
+
+################################
+######### streamlink ###########
+################################
+ln -s ~/repos/dotfiles/streamlink ~/.config/streamlink
+
+
+################################
+######### fzf ##################
+################################
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
