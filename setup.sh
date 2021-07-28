085a8828002b59064a00d73a2af587ac02481ac7
fap <fapdash@users.noreply.github.com>
2021-07-28 21:15:27 +0200

    Add bash setup & more

--- setup.sh
+++ setup.sh
@@ -21,8 +21,32 @@ ln -s ~/repos/dotfiles/streamlink ~/.config/streamlink
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+
 ################################
 ######### Ruby #################
 ################################
 ln -s ~/repos/dotfiles/ruby/.pryrc ~/.pryrc
 ln -s ~/repos/dotfiles/ruby/.irbrc ~/.irbrc
+
+
+################################
+######### Bash #################
+################################
+ln -s ~/repos/dotfiles/bash/.bashrc ~/.bashrc
+
+
+################################
+######### rbenv ################
+################################
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+# Optionally, try to compile dynamic bash extension to speed up rbenv.
+# Don't worry if it fails; rbenv will still work normally:
+# cd ~/.rbenv && src/configure && make -C src
+
+
+################################
+######### sdkman ###############
+################################
+curl -s "https://get.sdkman.io?rcupdate=false" | bash
