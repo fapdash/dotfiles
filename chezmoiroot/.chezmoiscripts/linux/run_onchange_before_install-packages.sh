de4fa5d5333c59f71f2fbae6c71473d1ffcf7af3
fap <459631+fapdash@users.noreply.github.com>
2024-08-21 11:32:05 +0200

    [chezmoi] Add pdf and latex utils

--- chezmoiroot/.chezmoiscripts/linux/run_onchange_before_install-packages.sh
+++ chezmoiroot/.chezmoiscripts/linux/run_onchange_before_install-packages.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-sudo apt install ripgrep\
-                 mpv
+sudo apt install -y ripgrep\
+                 mpv\
+                 img2pdf\
+                 pdftk-java\
+                 texlive\
+                 texlive-latex-extra\
+                 texlive-extra-utils\
+                 texlive-lang-german
