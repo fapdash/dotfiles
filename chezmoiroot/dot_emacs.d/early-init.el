8c1a56a6fbfe842a8aded014d917216fde4ab188
fap <459631+fapdash@users.noreply.github.com>
2024-11-14 16:34:31 +0100

    [emacs] Add early-init with font setup

---
+++ chezmoiroot/dot_emacs.d/early-init.el
@@ -0,0 +1,19 @@
+;; https://stackoverflow.com/questions/6026713/how-do-i-change-emacs-default-font-size-and-font-type
+(when initial-window-system
+  (add-to-list 'default-frame-alist '(font . "Cascadia Mono 9")))
+
+;; Experimenting with fonts:
+
+;; (set-frame-font "Ubuntu Sans Mono 10" nil t)
+
+;; (set-frame-font "DejaVu Sans Mono 9" nil t)
+
+;; (set-frame-font "Inconsolata 11" nil t)
+
+;; (set-frame-font "Cascadia Mono 9" nil t)
+
+;; (set-frame-font "Source Sans 3 11" nil t)
+
+;; (set-frame-font "Red Hat Mono 9" nil t)
+
+;; (set-frame-font "JetBrains Mono 8" nil t)
