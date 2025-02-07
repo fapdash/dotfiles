0dfec81a9bf78aa40be2bef596c8536c4dbc9086
fap <459631+fapdash@users.noreply.github.com>
2025-02-07 20:12:40 +0100

    Fix librewolf install script
    Old repo doesn't work anymore

--- chezmoiroot/.chezmoiscripts/linux/run_once_before-40-install-librewolf.sh
+++ chezmoiroot/.chezmoiscripts/linux/run_once_before-40-install-librewolf.sh
@@ -2,21 +2,8 @@
 
 set -eufo pipefail
 
-sudo apt update && sudo apt install -y wget gnupg lsb-release apt-transport-https ca-certificates
+sudo apt update && sudo apt install extrepo -y
 
-distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
+sudo extrepo enable librewolf
 
-wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
-
-sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
-Types: deb
-URIs: https://deb.librewolf.net
-Suites: $distro
-Components: main
-Architectures: amd64
-Signed-By: /usr/share/keyrings/librewolf.gpg
-EOF
-
-sudo apt update
-
-sudo apt install librewolf -y
+sudo apt update && sudo apt install librewolf -y
